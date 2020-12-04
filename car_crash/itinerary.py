d56eb51233e56b8808580e4424e521c3d02d0bce
Theo Paillusson <theo.paillusson@gmail.com>
2020-12-04 14:13:19 +0100

    version anterieure de initerary.py

--- car_crash/itinerary.py
+++ car_crash/itinerary.py
@@ -1,5 +1,6 @@
 # take gps coordinates and return name and distance
 import requests
+
 def itinerary(start_lon,start_lat,dest_lon,dest_lat):
     url = f'http://router.project-osrm.org/route/v1/driving/{start_lon},{start_lat};{dest_lon},{dest_lat}?overview=false&steps=true'
     response = requests.get(url).json()
@@ -14,3 +15,30 @@ def itinerary(start_lon,start_lat,dest_lon,dest_lat):
     del distances[-1]
     route = dict(zip(names,distances))
     return route
+
+
+# fonction pour tester le raccord à fast api
+def test_itinerary(start_lon,start_lat,dest_lon,dest_lat):
+    url = f'http://router.project-osrm.org/route/v1/driving/{start_lon},{start_lat};{dest_lon},{dest_lat}?overview=false&steps=true'
+    response = requests.get(url).json()
+    step = response['routes'][0]['legs'][0]['steps']
+    names = []
+    for i in range(len(step)):
+        names.append(response['routes'][0]['legs'][0]['steps'][i]['name'])
+    del names[-1]
+    distances = []
+    for i in range(len(step)):
+        distances.append(response['routes'][0]['legs'][0]['steps'][i]['distance'])
+    del distances[-1]
+    route = dict(zip(names,distances))
+    return route
+
+
+def convertion_gps():
+    pass 
+
+
+# get map
+def get_map():
+    '''function wich return a map or informations to generate a map with the itinerary'''
+    pass
