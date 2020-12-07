fca600ceb0fd81ebd05d3e94cadc89ad1326ae84
Theo Paillusson <theo.paillusson@gmail.com>
2020-12-07 10:28:08 +0100

    app déplacée dans le repo front

--- api/fast.py
+++ api/fast.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from car_crash.test_api import model_predict, user_data
 from app.app import return_inputs
-
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+# for api key
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+GOOGLE_MAP_API = os.environ.get('GOOGLE_MAP_API')
 
 app = FastAPI()
 
--- car_crash/itinerary.py
+++ car_crash/itinerary.py
@@ -8,29 +8,29 @@ def get_coordinates(departure, arrival, api):
     response1 = requests.get(url_start).json()
     response2 =requests.get(url_destination).json()
     return response1
-    # lon_start = response1['results'][0]['geometry']['location']['lng']
-    # lat_start = response1['results'][0]['geometry']['location']['lat']
-    # lon_end = response2['results'][0]['geometry']['location']['lng']
-    # lat_end = response2['results'][0]['geometry']['location']['lat']
-    # return lon_start, lat_start, lon_end, lat_end
+    lon_start = response1['results'][0]['geometry']['location']['lng']
+    lat_start = response1['results'][0]['geometry']['location']['lat']
+    lon_end = response2['results'][0]['geometry']['location']['lng']
+    lat_end = response2['results'][0]['geometry']['location']['lat']
+    return lon_start, lat_start, lon_end, lat_end
 
 def itinerary(coordinates):
-    ''' Takes GPS coordinates and return route in a dict {name:distance}'''
-    url = f'http://router.project-osrm.org/route/v1/driving/{coordinates[0]},{coordinates[1]};{coordinates[2]},{coordinates[3]}?overview=false&steps=true'
-    response = requests.get(url).json()
-    step = response['routes'][0]['legs'][0]['steps']
-    names = []
-    for i in range(len(step)):
-        names.append(response['routes'][0]['legs'][0]['steps'][i]['name'])
-    del names[-1]
-    distances = []
-    for i in range(len(step)):
-        distances.append(response['routes'][0]['legs'][0]['steps'][i]['distance'])
-    del distances[-1]
-    route = dict(zip(names,distances))
-    return route
+     ''' Takes GPS coordinates and return route in a dict {name:distance}'''
+     url = f'http://router.project-osrm.org/route/v1/driving/{coordinates[0]},{coordinates[1]};{coordinates[2]},{coordinates[3]}?overview=false&steps=true'
+     response = requests.get(url).json()
+     step = response['routes'][0]['legs'][0]['steps']
+     names = []
+     for i in range(len(step)):
+         names.append(response['routes'][0]['legs'][0]['steps'][i]['name'])
+     del names[-1]
+     distances = []
+     for i in range(len(step)):
+         distances.append(response['routes'][0]['legs'][0]['steps'][i]['distance'])
+     del distances[-1]
+     route = dict(zip(names,distances))
+     return route
 
-# get map
+# # get map
 def get_geojson(coordinates):
     '''Takes coordinates and return geojson'''
     url = f'http://router.project-osrm.org/route/v1/driving/{coordinates[0]},{coordinates[1]};{coordinates[2]},{coordinates[3]}?steps=true&geometries=geojson'
